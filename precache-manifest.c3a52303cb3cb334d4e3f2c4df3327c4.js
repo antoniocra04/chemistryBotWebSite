21c991b5b670e0619d0d4cb8a5fd535bb6191457
antoniocra04 <antoniocra04@gmail.com>
2020-05-16 20:11:49 +0600

    Updates

---
+++ precache-manifest.c3a52303cb3cb334d4e3f2c4df3327c4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de3b3f024d2981491f303c7766b5f64",
+    "url": "/chemistryBotWebSite/index.html"
+  },
+  {
+    "revision": "6ed405e8054b704b9018",
+    "url": "/chemistryBotWebSite/static/css/main.73197821.chunk.css"
+  },
+  {
+    "revision": "cefbc5605349dc55e0b4",
+    "url": "/chemistryBotWebSite/static/js/2.55ea5ed2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chemistryBotWebSite/static/js/2.55ea5ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed405e8054b704b9018",
+    "url": "/chemistryBotWebSite/static/js/main.2de2da2a.chunk.js"
+  },
+  {
+    "revision": "ac87265428a42f545cdc",
+    "url": "/chemistryBotWebSite/static/js/runtime-main.f6ede65f.js"
+  },
+  {
+    "revision": "226d519086a3835e16c87f4ab1cd5641",
+    "url": "/chemistryBotWebSite/static/media/GothamPro-Black.226d5190.woff2"
+  },
+  {
+    "revision": "ad1867c1adc22645240b3fd33a205f9e",
+    "url": "/chemistryBotWebSite/static/media/GothamPro-Bold.ad1867c1.woff2"
+  },
+  {
+    "revision": "964e1508f92be294b2d5be7e8ef64192",
+    "url": "/chemistryBotWebSite/static/media/GothamPro-Light.964e1508.woff2"
+  },
+  {
+    "revision": "032df678e57acf963dcfc62625bb61e0",
+    "url": "/chemistryBotWebSite/static/media/GothamPro-Medium.032df678.woff2"
+  },
+  {
+    "revision": "f02e0f6b007b64a0567060a299cedf4e",
+    "url": "/chemistryBotWebSite/static/media/GothamPro-Reg.f02e0f6b.woff2"
+  },
+  {
+    "revision": "2b1df315ae053825eb2a2ae51200ddd4",
+    "url": "/chemistryBotWebSite/static/media/arrow.2b1df315.svg"
+  },
+  {
+    "revision": "da749189bed8ce119d1dd0f0ba9bfd56",
+    "url": "/chemistryBotWebSite/static/media/calc.da749189.svg"
+  },
+  {
+    "revision": "e737497a6beb95b00a13ab5c72290509",
+    "url": "/chemistryBotWebSite/static/media/fx.e737497a.svg"
+  },
+  {
+    "revision": "0a0f456355599756a5443d56b74a8135",
+    "url": "/chemistryBotWebSite/static/media/illustration.0a0f4563.png"
+  },
+  {
+    "revision": "8562ad58b73f3d7c9a7c78ee29a71a86",
+    "url": "/chemistryBotWebSite/static/media/info.8562ad58.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chemistryBotWebSite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8ae0ee6fd39205dcc921537c452c3d0",
+    "url": "/chemistryBotWebSite/static/media/logo.f8ae0ee6.svg"
+  },
+  {
+    "revision": "68055b32718b78214fd08aa8fc2f99d8",
+    "url": "/chemistryBotWebSite/static/media/menu.68055b32.svg"
+  },
+  {
+    "revision": "ee47d99641c2aa0def72f359471c87e0",
+    "url": "/chemistryBotWebSite/static/media/part1.ee47d996.png"
+  },
+  {
+    "revision": "16c5c23f0cb665cf1ace44a25e5a0047",
+    "url": "/chemistryBotWebSite/static/media/part2.16c5c23f.png"
+  },
+  {
+    "revision": "cd4f15eb9f472d83dc0fcc2ee285186a",
+    "url": "/chemistryBotWebSite/static/media/table.cd4f15eb.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/chemistryBotWebSite/precache-manifest.c3a52303cb3cb334d4e3f2c4df3327c4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chemistryBotWebSite/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
